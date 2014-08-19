6968c0db5d46f6651b390b51f86c758166fe2ff2
Vitaly Repin <vitaly.repin@gmail.com>
2014-08-19 22:25:22 +0300

    template RuEn for 2015 was added

---
+++ calendars/Trivial2015/custom.mk
@@ -0,0 +1,4 @@
+# Makefile customization
+
+IMAGES=
+
---
+++ templates/RuEnPhoto2015/custom.mk
@@ -0,0 +1,4 @@
+# Makefile customization
+
+IMAGES=$(addprefix photos/, jan.png feb.png mar.png apr.png may.png jun.png jul.png aug.png sep.png oct.png nov.png dec.png front.png back.png)
+
