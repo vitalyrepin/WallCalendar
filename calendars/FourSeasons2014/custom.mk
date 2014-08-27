92e5e2dcabc22b95737b3dcb2353161d3775b1c6
Vitaly Repin <vitaly.repin@gmail.com>
2014-08-27 17:08:18 +0300

    Directory reorg. To be able to use photos from different calendars

--- calendars/FourSeasons2014/custom.mk
+++ calendars/FourSeasons2014/custom.mk
@@ -1,4 +1,4 @@
 # Makefile customization
 
-IMAGES=$(addprefix photos/, baikalsky.png espoo.png hoboi.png irkutsk.png kapchuk.png nuuksio.png peterhof.png piter.png porkkala.png riga.png ruskeala.png savonlinna.png saima.png zunmurino.png)
+IMAGES=$(addprefix ../../resources/photos2014/, baikalsky.png espoo.png hoboi.png irkutsk.png kapchuk.png nuuksio.png peterhof.png piter.png porkkala.png riga.png ruskeala.png savonlinna.png saima.png zunmurino.png)
 
