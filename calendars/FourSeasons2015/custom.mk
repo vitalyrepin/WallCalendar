f394d11722fa8e3ac6ef964342a8c80fdf2eebb2
Vitaly Repin <vitaly.repin@gmail.com>
2014-08-28 00:03:58 +0300

    Four Seasons 2015

---
+++ calendars/FourSeasons2015/custom.mk
@@ -0,0 +1,4 @@
+# Makefile customization
+
+IMAGES=$(addprefix ../../resources/photos2014/, baikalsky.png espoo.png hoboi.png irkutsk.png kapchuk.png nuuksio.png peterhof.png piter.png porkkala.png riga.png ruskeala.png savonlinna.png saima.png zunmurino.png)
+
